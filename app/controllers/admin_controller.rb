db3f7fb72e29f3c397157ce659cacc4ec009dd1d
Lia Kitamura <kit4mura@gmail.com>
2014-07-11 10:32:37 -0600

    added in some notes about routes and controller stuff

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,4 @@
+# parent controller. provides stuff to its children classes
 class AdminController < ApplicationController
   before_action :authenticate_user!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root 'site#index'
   get '/about' => 'site#about'
-  get 'contact' => 'site#contact'
+  get '/contact' => 'site#contact'
   post '/contact-submit' => 'site#contact_submit', as: :submit_contact
+  # public can only access index and show
   resources :menu_items, only: [:index, :show], path: 'our-food'
   get '/our-vegetarian-food' => 'menu_items#vegetarian'
 
+  # namespacing is pretty much just for organization. also restricts public from doing unwanted stuff.
   namespace :admin do
     get '/' => 'base#index'
-
+    # namespaced so admin can access all menu_items
     resources :menu_items
   end
 
